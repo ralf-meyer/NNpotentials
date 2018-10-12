269967cc175355b2932876627616ed088b3ffeab
Ralf Meyer <ralf_meyer@cuda02.tugraz.at>
2018-10-12 09:21:02 +0200

    CHG: fixed missing parentesis

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import tensorflow as _tf
 
 def morse(x, lamb = 0.23314621043800202, alpha = 0.6797779934458726):
-    return lamb*(_tf.exp(-2.0*alpha*(x))-2.0*_tf.exp(-alpha*(x))
+    return lamb*(_tf.exp(-2.0*alpha*(x))-2.0*_tf.exp(-alpha*(x)))
 
 def stash(x, lamb = 1.1613855392326946, alpha = 0.6520042387583171):
     return _tf.where(x <= 0.0, 2.0*_tf.tanh(alpha*x),
