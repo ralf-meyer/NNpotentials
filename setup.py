205729461a281fc38d68276fa9d7e4c949843402
Ralf Meyer <ralf.meyer@tugraz.at>
2018-11-26 15:24:14 +0100

    CHG: Moved requirement to seperate files to avoid messing up tensorflow

--- setup.py
+++ setup.py
@@ -5,6 +5,5 @@ setup(
     version='1.1',
     description='Package for fitting Neural Network potentials to ab initio data',
     author='Ralf Meyer',
-    packages=['NNpotentials'],
-    install_requires=['numpy', 'tensorflow']
+    packages=['NNpotentials']
  )
